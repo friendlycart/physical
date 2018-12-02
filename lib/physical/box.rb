d7d1ada2560a2ced4d37d0bb1c4422dde7a800d3
Martin Meyerhoff <mamhoff@gmail.com>
2018-12-02 23:01:31 +0100

    Fill up dimensions with Infinite Length
    We will have a need for boxes that are infinitely large. Providing
    BigDecimal::INFINITY as the default size for a box neatly allows this.

--- lib/physical/box.rb
+++ lib/physical/box.rb
@@ -5,8 +5,9 @@ module Physical
   class Box
     attr_reader :dimensions
 
-    def initialize(dimensions, unit)
+    def initialize(dimensions = [], unit = :cm)
       @dimensions = dimensions.map { |dimension| Measured::Length.new(dimension, unit) }
+      @dimensions.fill(Measured::Length(BigDecimal::INFINITY, unit), @dimensions.length..2)
     end
   end
 end
--- spec/physical/box_spec.rb
+++ spec/physical/box_spec.rb
@@ -13,4 +13,46 @@ RSpec.describe Physical::Box do
       ]
     )
   end
+
+  context "when given a one-element dimensions array" do
+    let(:args) { [[2], :cm] }
+
+    specify "the other dimensions are filled up with BigDecimal::INFINITY" do
+      expect(subject.dimensions).to eq(
+        [
+          Measured::Length.new(2, :cm),
+          Measured::Length.new(BigDecimal::INFINITY, :cm),
+          Measured::Length.new(BigDecimal::INFINITY, :cm)
+        ]
+      )
+    end
+  end
+
+  context "when given a two-element dimensions array" do
+    let(:args) { [[1, 2], :cm] }
+
+    it "the last dimension is filled up with BigDecimal::INFINITY" do
+      expect(subject.dimensions).to eq(
+        [
+          Measured::Length.new(1, :cm),
+          Measured::Length.new(2, :cm),
+          Measured::Length.new(BigDecimal::INFINITY, :cm)
+        ]
+      )
+    end
+  end
+
+  context "when given no arguments" do
+    let(:args) { [] }
+
+    it "assumes cm as the unit and infinity as value" do
+      expect(subject.dimensions).to eq(
+        [
+          Measured::Length.new(BigDecimal::INFINITY, :cm),
+          Measured::Length.new(BigDecimal::INFINITY, :cm),
+          Measured::Length.new(BigDecimal::INFINITY, :cm)
+        ]
+      )
+    end
+  end
 end
