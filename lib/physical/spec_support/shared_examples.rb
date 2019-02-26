68964cd0c94409eaf02088a3fdc84e638a837844
Martin Meyerhoff <mamhoff@gmail.com>
2019-02-26 14:57:14 +0100

    Do not convert values when initializing packages/cuboids
    We let the conversion be handled by the `measured` gem instead.

--- lib/physical/spec_support/shared_examples.rb
+++ lib/physical/spec_support/shared_examples.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a cuboid' do
-  let(:args) { {dimensions: [1, 2, 3], dimension_unit: :cm} }
+  let(:args) { {dimensions: [1.1, 2.2, 3.3], dimension_unit: :cm} }
 
   it { is_expected.to be_a(Physical::Cuboid) }
 
-  it "has dimensions as Measured::Length objects" do
+  it "has dimensions as Measured::Length objects with rational values" do
     expect(subject.dimensions).to eq(
       [
-        Measured::Length.new(1, :cm),
-        Measured::Length.new(2, :cm),
-        Measured::Length.new(3, :cm)
+        Measured::Length.new(1.1, :cm),
+        Measured::Length.new(2.2, :cm),
+        Measured::Length.new(3.3, :cm)
       ]
     )
   end
 
   it "has getter methods for each dimension as Measured::Length object" do
-    expect(subject.width).to eq(Measured::Length.new(1, :cm))
-    expect(subject.height).to eq(Measured::Length.new(2, :cm))
-    expect(subject.depth).to eq(Measured::Length.new(3, :cm))
+    expect(subject.width).to eq(Measured::Length.new(1.1, :cm))
+    expect(subject.height).to eq(Measured::Length.new(2.2, :cm))
+    expect(subject.depth).to eq(Measured::Length.new(3.3, :cm))
   end
 end
