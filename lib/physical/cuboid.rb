2c2dbf618e361da90c745c5ff7f4beb123aa7268
Martin Meyerhoff <mamhoff@gmail.com>
2019-05-13 08:18:54 +0200

    Define Length, Width, and Height as relative sizes
    According to ActiveShipping's source code, length, width and height are
    defined as the longest, middle, and shortest dimension of a Cuboid. See
    https://github.com/Shopify/active_shipping/blob/master/lib/active_shipping/package.rb#L154-L156
    and
    https://github.com/Shopify/active_shipping/blob/master/lib/active_shipping/package.rb#L143-L145
    for "proof".

--- lib/physical/cuboid.rb
+++ lib/physical/cuboid.rb
@@ -4,16 +4,21 @@ require 'measured'
 
 module Physical
   class Cuboid
-    attr_reader :dimensions, :width, :height, :depth, :weight, :id, :properties
+    attr_reader :dimensions, :length, :width, :height, :weight, :id, :properties
 
     def initialize(id: nil, dimensions: [], dimension_unit: :cm, weight: 0, weight_unit: :g, properties: {})
       @id = id || SecureRandom.uuid
       @weight = Measured::Weight(weight, weight_unit)
-      @dimensions = dimensions.map { |dimension| Measured::Length.new(dimension, dimension_unit) }
+      @dimensions = dimensions.map { |dimension| Measured::Length.new(dimension, dimension_unit) }.sort
       @dimensions.fill(Measured::Length(self.class::DEFAULT_LENGTH, dimension_unit), @dimensions.length..2)
-      @width, @height, @depth = *@dimensions
+      @length, @width, @height = *@dimensions.reverse
       @properties = properties
     end
+
+    alias :x :length
+    alias :y :width
+    alias :z :height
+    alias :depth :height
 
     def volume
       Measured::Volume(dimensions.map { |d| d.convert_to(:cm).value }.reduce(1, &:*), :ml)
