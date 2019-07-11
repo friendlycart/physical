9f68608b25271810b1c749b2e9871a358fb5abd9
Thomas von Deyen <thomas@vondeyen.com>
2019-07-11 17:35:02 +0200

    Add max_weight to Physical::Box
    If you want to do weight based item distribution with
    `Physical::Package`s we need to have the ability to set
    a `max_weight` on containers.

--- lib/physical/box.rb
+++ lib/physical/box.rb
@@ -5,15 +5,19 @@ require 'measured'
 module Physical
   class Box < Cuboid
     DEFAULT_LENGTH = BigDecimal::INFINITY
+    DEFAULT_MAX_WEIGHT = BigDecimal::INFINITY
+
     attr_reader :inner_dimensions,
                 :inner_length,
                 :inner_width,
-                :inner_height
+                :inner_height,
+                :max_weight
 
-    def initialize(inner_dimensions: [], **args)
+    def initialize(inner_dimensions: [], max_weight: Measured::Weight(DEFAULT_MAX_WEIGHT, :g), **args)
       super args
       @inner_dimensions = fill_dimensions(Types::Dimensions[inner_dimensions])
       @inner_length, @inner_width, @inner_height = *@inner_dimensions
+      @max_weight = Types::Weight[max_weight]
     end
 
     def inner_volume
