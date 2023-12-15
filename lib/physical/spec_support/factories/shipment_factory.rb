b0e15f08fbf8175acc6107be606a6f8308c11161
Matthew Bass <matt@candlescience.com>
2023-12-15 15:10:39 -0500

    Update `Physical::Shipment` to accept structures
    Instead of passing pallets to `Physical::Shipment` we should be
    passing structures instead. Structures could be pallets, skids, or
    anything else that contains one or more packages.
    
    The idea is to support modeling shipments containing one or more
    pallets (each of which holds one or more packages) in addition to
    separate packages that aren't being shipped on a pallet.
    
    This commit deprecates the `pallets` argument.

--- lib/physical/spec_support/factories/shipment_factory.rb
+++ lib/physical/spec_support/factories/shipment_factory.rb
@@ -4,9 +4,14 @@ FactoryBot.define do
   factory :physical_shipment, class: "Physical::Shipment" do
     origin { FactoryBot.build(:physical_location) }
     destination { FactoryBot.build(:physical_location) }
-    pallets { build_list(:physical_pallet, 1) }
+    pallets { build_list(:physical_pallet, 1) } # deprecated, will be removed
     packages { build_list(:physical_package, 2) }
     service_code { "usps_priority_mail" }
     initialize_with { new(**attributes) }
+
+    trait :freight do
+      structures { build_list(:physical_structure, 1) }
+      service_code { "tforce_freight" }
+    end
   end
 end
--- spec/physical/shipment_spec.rb
+++ spec/physical/shipment_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Physical::Shipment do
   let(:args) { {} }
   subject(:shipment) { described_class.new(**args) }
 
-  describe '#pallets' do
-    subject { shipment.pallets }
+  describe '#structures' do
+    subject { shipment.structures }
 
     it { is_expected.to eq([]) }
   end
@@ -40,15 +40,36 @@ RSpec.describe Physical::Shipment do
     it { is_expected.to eq({}) }
   end
 
-  describe 'factory' do
+  describe '#packages' do
+    subject { shipment.packages }
+
+    let(:args) { { packages: packages } }
+    let(:packages) { [Physical::Package.new(id: "1"), Physical::Package.new(id: "2")] }
+
+    it { is_expected.to eq(packages) }
+  end
+
+  describe "factory" do
     subject { FactoryBot.build(:physical_shipment) }
 
     it "works" do
       expect(subject.origin).to be_a(Physical::Location)
       expect(subject.destination).to be_a(Physical::Location)
-      expect(subject.pallets.length).to eq(1)
+      expect(subject.structures).to be_empty
       expect(subject.packages.length).to eq(2)
       expect(subject.service_code).to eq("usps_priority_mail")
+    end
+
+    describe "freight" do
+      subject { FactoryBot.build(:physical_shipment, :freight) }
+
+      it "works" do
+        expect(subject.origin).to be_a(Physical::Location)
+        expect(subject.destination).to be_a(Physical::Location)
+        expect(subject.structures.length).to eq(1)
+        expect(subject.packages.length).to eq(2)
+        expect(subject.service_code).to eq("tforce_freight")
+      end
     end
   end
 end
